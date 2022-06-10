a2d580e3be0295de8d289d89de84724831170180
vostavhy <vostavhy@gmail.com>
2022-06-11 02:26:46 +0300

    feat: implement add command

--- files/add.js
+++ files/add.js
@@ -3,5 +3,9 @@ import path from 'path';
 
 export const createFile = (currentDir, fileName) => {
   const filePath = path.join(currentDir, fileName);
-  createWriteStream(filePath);
+  const ws = createWriteStream(filePath, { flags: 'ax' });
+  ws.on('error', () =>
+    process.stdout.write(`File already exists: ${filePath} \n`)
+  );
+  ws.end();
 };

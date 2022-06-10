3294bec8600e754384bd10ac7ddd30c0eabb5d92
vostavhy <vostavhy@gmail.com>
2022-06-11 02:31:16 +0300

    refactor: rewrite error message for file add command

--- files/add.js
+++ files/add.js
@@ -1,11 +1,14 @@
 import { createWriteStream } from 'fs';
+import { OPERATION_FAILED } from '../utils/constants.js';
 import path from 'path';
 
 export const createFile = (currentDir, fileName) => {
   const filePath = path.join(currentDir, fileName);
   const ws = createWriteStream(filePath, { flags: 'ax' });
   ws.on('error', () =>
-    process.stdout.write(`File already exists: ${filePath} \n`)
+    process.stdout.write(
+      `${OPERATION_FAILED}File already exists: ${filePath} \n`
+    )
   );
   ws.end();
 };
--- files/cat.js
+++ files/cat.js
@@ -1,17 +1,11 @@
 import { createReadStream } from 'fs';
-import { access } from 'fs/promises';
 import { OPERATION_FAILED } from '../utils/constants.js';
 import path from 'path';
 
 export const readFile = async (currentDir, pathToFile) => {
   const filePath = path.join(currentDir, pathToFile);
-
-  try {
-    await access(filePath);
-    const input = createReadStream(filePath);
-    input.pipe(process.stdout);
-    process.stdout.write('\n');
-  } catch {
-    process.stdout.write(OPERATION_FAILED);
-  }
+  const input = createReadStream(filePath);
+  input.on('error', () => process.stdout.write(OPERATION_FAILED));
+  input.pipe(process.stdout);
+  process.stdout.write('\n');
 };
